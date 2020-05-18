35a7a874002677a8abb68a015071fe86241ead58
IF <iisaquielfernandes@gmail.com>
2020-05-18 16:08:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 
 ScrollReveal().reveal('.container', { delay: 500 });
+
+
+.fluidbox-overlay {
+	    .overlay-1 & {
+	        background-color: rgba(255,190,78,.85);
+	    }
+	    .overlay-2 & {
+	        background-color: transparent; /* To override default style */
+	        background-image: linear-gradient(
+	            to top left,
+	            rgba(130,168,158,0.85),
+	            rgba(134,150,173,.85)
+	        );
+	    }
+}
