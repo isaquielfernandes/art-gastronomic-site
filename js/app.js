3ca0175c7776578e87fae6a17dcefb43e38075c8
IF <iisaquielfernandes@gmail.com>
2020-05-19 17:22:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,14 +4,6 @@ ScrollReveal().reveal('.container', { delay: 500 });
 // Apply Fluidbox to elements of interest
 $('.card img').fluidbox();
 
-var $caption = $('<div />', { 'id': 'custom-trigger-1-social' });
-$caption
-   .html('<div class="img-caption"></div><p class="img-desc"></p><ul><li>Share on:</li><li><a href="#">Facebook</a></li><li><a href="#">Pinterest</a></li><li><a href="#">Twitter</a></li></ul>')
-   .appendTo($('body'));
-$(document).on('click', '#custom-trigger-1-social', function(e) {
-    e.preventDefault();
-});
-
 // Initialize Fluidbox
 $('.card img').fluidbox()
      .on('openend.fluidbox', function() {
