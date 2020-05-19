9c54f784291bd3cf63b6b0e6695f3d3b825990f9
IF <iisaquielfernandes@gmail.com>
2020-05-20 00:54:15 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ class UI{
        result += `
           <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ${receita.categoria}" data-fluidbox>
                  <div class="card mb-3">
-                      <img src=${receita.imagem} class="card-img-top img-fluid" alt="receita-img" >
+                      <img src=${receita.image} class="card-img-top img-fluid" alt="receita-img" >
                       <div class="card-body p-1">
                         <h5 class="card-title mt-1">${receita.nome}</h5>
                         <p class="card-text text-truncate">${receita.descricao}</p>
