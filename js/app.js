ae67c668fb708a2c1cd1b959fcc8154e2e5b6d82
IF <iisaquielfernandes@gmail.com>
2020-05-20 16:22:27 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,30 +8,3 @@ ScrollReveal().reveal('.container', { delay: 500 });
 // Apply Fluidbox to elements of interest
 $('.card img').fluidbox();
 
-// Initialize Fluidbox
-$('.card img').fluidbox()
-     .on('openend.fluidbox', function() {
-          var $img = $(this).find('img');
-})
-.on('closestart.fluidbox', function() {
-    
-});
-
-// Call public methods
-$(window).scroll(function() {
-	$('.card img').fluidbox('close');
-});
-
-.fluidbox-overlay {
-	    .overlay-1 & {
-	        background-color: rgba(255,190,78,.85);
-	    }
-	    .overlay-2 & {
-	        background-color: transparent; /* To override default style */
-	        background-image: linear-gradient(
-	            to top left,
-	            rgba(130,168,158,0.85),
-	            rgba(134,150,173,.85)
-	        );
-	    }
-}
