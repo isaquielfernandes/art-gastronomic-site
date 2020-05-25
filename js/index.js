2f4bc3ecff53695aef9bec727d489d8226fed2fe
IF <iisaquielfernandes@gmail.com>
2020-05-25 13:42:49 +0100

    commit ajuste

--- js/index.js
+++ js/index.js
@@ -40,18 +40,18 @@ class UI{
      let result = "";
      receitas.forEach(receita => {
        result += `
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ${receita.categoria}" data-fluidbox>
-              <article>
-                  <div class="card mb-3 img-container">
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 p-2 mb-0 ${receita.categoria}" data-fluidbox>
+                <article>
+                  <div class="card img-container">
                       <img src=${receita.image} class="card-img-top img-fluid" alt="receita-img" >
                       <button type="button" class="btn-ver-receita"data-id="${receita.id}" data-toggle="modal" data-target="">
                           Ver Receita <i class="fa fa-cutlery" aria-hidden="true"></i>
                       </button>
-                      <div class="card-body p-2">
+                      <div class="card-header p-2">
                         <h5 class="card-title mt-1">${receita.nome}</h5>
                       </div>
                   </div>
-              </article>
+                </article>
           </div>
        `;
      });
