349194e2e32fb98bd1c4843b10829d9e6b7c399b
IF <iisaquielfernandes@gmail.com>
2020-05-26 12:20:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ class UI{
         result +=  `
                  <article class="col-sm-6 col-md-4 col-lg-4 p-2 mb-0 ${receita.categoria}">
                    <div class="card img-container">
-                       <img src=${receita.image} class="card-img-top img-fluid receita-img" alt="receita-img" loading="lazy">
+                       <img src=${receita.image} class="card-img-top img-fluid receita-img" alt="receita-img" >
                        <button type="button" class="btn-ver-receita" data-id=${receita.id} data-toggle="" data-target="">
                            Ver Receita <i class="fa fa-cutlery"></i>
                        </button>
